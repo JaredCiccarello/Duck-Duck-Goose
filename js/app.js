77e2b8950e7ba21001cf8e6fabdbbcfcbc317877
Jared Ciccarello <Jared.Ciccarello@gmail.com>
2023-04-19 20:11:39 -0400

    Updated website

--- js/app.js
+++ js/app.js
@@ -1,4 +1,33 @@
 let duckArray = [];
+let DuckArrayFromStorage = localStorage.getItem('duckArray');
+if (DuckArrayFromStorage){
+  duckArray = JSON.parse(DuckArrayFromStorage);
+} else {
+  // These are your instances: 19
+  let bag = new Duck('bag');
+  let banana = new Duck('banana');
+  let bathroom = new Duck('bathroom');
+  let boots = new Duck('boots');
+  let breakfast = new Duck('breakfast');
+  let bubblegum = new Duck('bubblegum');
+  let chair = new Duck('chair');
+  let cthulhu = new Duck('cthulu');
+  let dogduck = new Duck('dogduck');
+  let dragon = new Duck('dragon');
+  let pen = new Duck('pen');
+  let petsweep = new Duck('petsweep');
+  let scissors = new Duck('scissors');
+  let shark = new Duck('shark');
+  // What's going on here???
+  let sweep = new Duck('sweep', 'png');
+  let tauntaun = new Duck('tauntaun');
+  let unicorn = new Duck('unicorn');
+  let watercan = new Duck('watercan');
+  let wineglass = new Duck('wineglass');
+  
+  duckArray.push(bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogduck, dragon, pen, petsweep, scissors, shark, sweep, tauntaun, unicorn, watercan, wineglass);
+}
+
 let imageIndex = [];
 
 let myContainer = document.querySelector('section');
@@ -10,7 +39,7 @@ let image3 = document.querySelector('section img:nth-child(3)');
 let viewResultsBtn = document.querySelector('section ~ div')
 
 let counter = 0;
-let maxCounter = 5;
+let maxCounter = 25;
 
 function Duck(name, fileExtension = 'jpg') {
   this.name = name;
@@ -18,29 +47,6 @@ function Duck(name, fileExtension = 'jpg') {
   this.views = 0;
   this.votes = 0;
 }
-// These are your instances: 19
-let bag = new Duck('bag');
-let banana = new Duck('banana');
-let bathroom = new Duck('bathroom');
-let boots = new Duck('boots');
-let breakfast = new Duck('breakfast');
-let bubblegum = new Duck('bubblegum');
-let chair = new Duck('chair');
-let cthulhu = new Duck('cthulu');
-let dogduck = new Duck('dogduck');
-let dragon = new Duck('dragon');
-let pen = new Duck('pen');
-let petsweep = new Duck('petsweep');
-let scissors = new Duck('scissors');
-let shark = new Duck('shark');
-// What's going on here???
-let sweep = new Duck('sweep', 'png');
-let tauntaun = new Duck('tauntaun');
-let unicorn = new Duck('unicorn');
-let watercan = new Duck('watercan');
-let wineglass = new Duck('wineglass');
-
-duckArray.push(bag, banana, bathroom, boots, breakfast, bubblegum, chair, cthulhu, dogduck, dragon, pen, petsweep, scissors, shark, sweep, tauntaun, unicorn, watercan, wineglass);
 
 // console.log(duckArray);
 
@@ -52,7 +58,7 @@ function selectRandomDuckNumber() {
 // - update the DOM (change duck photos in the DOM)
 //     - increment the number of views on the ducks
 function renderDucks() {
-  
+
   while (imageIndex.length < 6) {
     // get random number included in duck array
     let random = selectRandomDuckNumber();
@@ -71,11 +77,11 @@ function renderDucks() {
   let duck3 = imageIndex.shift(); // 5
   console.log(duck1, duck2, duck3);
   // as long as duck1 === duck2 we need a new number for duck2
-    let selectImage = []
+  let selectImage = []
 
   while (duck1 === duck2) {
     duck2 = selectRandomDuckNumber(); // 5
-    console.log(duck1, duck2);0
+    console.log(duck1, duck2); 0
   }
 
   // seriously consider using an array — you can push things to an array
@@ -91,7 +97,7 @@ function renderDucks() {
   image3.src = duckArray[duck3].src;
   image3.alt = duckArray[duck3].name;
   duckArray[duck3].views++;
-  
+
 }
 
 // - handle duck clicks
@@ -120,14 +126,19 @@ function handleDuckClick(event) {
     viewResultsBtn.addEventListener('click', viewResults);
     // stop the game and render the results
   }
+
+  let stringifiedDuckArray = JSON.stringify(duckArray);
+  localStorage.setItem('duckArray', stringifiedDuckArray);
 }
+
 function viewResults() {
-  let ul = document.querySelector('ul');
-  for (let i = 0; i < duckArray.length; i++) {
-    let li = document.createElement('li');
-    li.textContent = `${duckArray[i].name} had ${duckArray[i].views} views and ${duckArray[i].votes} votes.`;
-    ul.appendChild(li);
-  }
+  // let ul = document.querySelector('ul');
+  // for (let i = 0; i < duckArray.length; i++) {
+  //   let li = document.createElement('li');
+  //   li.textContent = `${duckArray[i].name} had ${duckArray[i].views} views and ${duckArray[i].votes} votes.`;
+  //   ul.appendChild(li);
+  // }
+  renderChart();
 }
 renderDucks();
 
@@ -137,28 +148,119 @@ myContainer.addEventListener('click', handleDuckClick);
 
 
 
-// We use render because ???? 
+// We use render to cause chart to appear
 function renderChart() {
-  console.log(goatArray);
-  
+
   // When using render, we need a window into the DOM
-  const ctx = document.getElementById('myChart');
-  
+  // const ctx = document.getElementById('myChart');
+
   let duckNames = []
   let duckVotes = []
   let duckViews = []
-  
-  for (let i = 0; i < duckArray.length; i++)
-  console.log(goatArray[i]);
-  
-  // we use let for name because ????
-  let name = duckArray[i].name;
-  goatNames.push(name);
-  // Using .push takes values received from the user and pushes this information back to the array
-  duckVotes.push(duckArray[i].votes);
-  duckViews.push(duckArray[i].views);
-  
+
+  for (let i = 0; i < duckArray.length; i++) {
+
+
+    // we use let for name because ????
+    let name = duckArray[i].name;
+    duckNames.push(name);
+    // Using .push takes values received from the user and pushes this information back to the array
+    duckVotes.push(duckArray[i].votes);
+    duckViews.push(duckArray[i].views);
+  }
+
+  const data = {
+    type: 'bar',
+    data: {
+      labels: duckNames,
+      datasets: [{
+        label: 'votes',
+        data: duckVotes,
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)'
+        ],
+        borderColor: [
+          'rgb(255, 99, 132)'
+        ],
+        borderWidth: 1
+      },
+      {
+        label: 'Views',
+        data: duckViews,
+        backgroundColor: [
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgb(255, 159, 64)'
+        ],
+        borderWidth: 1
+      }]
+    },
+
+    // const config = {
+    //   type: 'bar',
+    //   data: data,
+      options: {
+        scales: {
+          y: {
+            beginAtZero: true
+          }
+        }
+      },
+    };
+
+    let canvasChart = document.getElementById('myChart');
+    let chart = new Chart (canvasChart, data)
+  //   const myChart = new Chart(canvasChart, config);
+  // }
 }
+// renderChart()
+
+
+// const config = {
+//   type: 'bar',
+//   data: {
+//     labels: duckNames,
+//     datasets: [
+//       {
+//         label: '# of Votes',
+//         data: duckVotes,
+//         borderWidth: 1,
+//         backgroundColor: [
+//           'rgba(255, 99, 132, 0.2)',
+//           'rgba(255, 159, 64, 0.2)'
+//         ],
+//         borderColor: [
+//           'rgb(255, 99, 132)',
+//           'rgb(255, 159, 64)'
+//         ]
+//       },
+//       {
+//         label: '# of Views',
+//         data: duckViews,
+//         borderWidth: 1,
+//         backgroundColor: [
+//           'rgba(255, 99, 132, 0.2)',
+//           'rgba(255, 159, 64, 0.2)'
+//         ],
+//         borderColor: [
+//           'rgb(255, 99, 132)',
+//           'rgb(255, 159, 64)'
+//         ]
+//       }
+//     ]
+//   },
+//   options: {
+//     scales: {
+//       y: {
+//         beginAtZero: true
+//       }
+//     }
+//   }
+// };
+// new Chart(ctx, config);
+
+
 renderDucks();
 
 myContainer.addEventListener('click', handleDuckClick);
